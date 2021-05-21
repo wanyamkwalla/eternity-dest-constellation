0a7b50c6625503dcce9dc26d0086a916520d97c0
hiro961227 <70287260+wanyamkwalla@users.noreply.github.com>
2021-05-21 18:11:15 +0900

    updata

---
+++ event.js
@@ -0,0 +1,59 @@
+$(function(){ 
+    //s
+    $.ajax({
+        url:'data.xml',
+        type:'GET',
+        success:function(data){
+  
+            var imgSrc,name,nihongo;
+            function funList(code){
+                var cute='',popup;
+                $(data).find('item').each(function(i){
+  
+                    imgSrc = $(this).find('imgSrc').text();
+                    name = $(this).find('name').text();
+                    nihongo = $(this).find('nihongo').text();
+                      
+                    cute += "<li data-num='"+i+"' id='"+name+"'>";
+                    cute += "<img src='"+imgSrc+"' alt='"+name+"'>";
+                    cute += "</li>";
+                    
+                    if(Number(code) == i){
+                        popup += "<div data-num='"+i+"' class='cute'>";
+                        popup += "<img src='"+imgSrc+"' alt='"+name+"'>";
+                        popup += "</div>";
+                        popup += "<span class='text-vertical-1'>"+nihongo+"</span>";
+                        
+                        $('#popup .open_popup').html(popup);  
+                    }
+                    if(code == 'all'){
+                        $('#youngjong ul').html(cute);
+                    }   
+       
+                });
+
+                 //popup 
+                var suki = $('#youngjong ul li');
+                var popupOpen = $('#popup');
+                var popupClose = $('#popup .close');
+  
+                 //클릭 이벤트
+                suki.click(function(){
+                    // if(cute.attr('data-num') == popup.attr('data-num')){
+                    //     popupOpen.css({display:"block",opacity:"0"}).animate({'opacity': 1}, 250);    
+                    // }        
+                    funList($(this).attr('data-num'));
+                    popupOpen.css({display:"block",opacity:"0"}).animate({'opacity': 1}, 250);    
+                });
+                popupClose.click(function(){
+                    popupOpen.css({display:"none",opacity:"0"});
+                });  
+            //
+            }
+            funList('all');    
+                     
+        //
+        }
+    });
+    //e
+  })
